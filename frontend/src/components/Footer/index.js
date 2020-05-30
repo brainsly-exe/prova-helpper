a4da100c6dd9a77bce2b646cc1fa2a1bbe629364
PH-WebDeveloper <pedrobatutu@gmail.com>
2020-05-30 17:23:29 -0300

    Adicionado link do portfólio ao footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -4,11 +4,15 @@ import { FiLoader } from 'react-icons/fi'
 import './styles.css'
 
 export default function Footer() {
-    return(
+    return (
         <div className="footer-container">
             <div className="credits">
-                <FiLoader color="#E02041" size={18}/>
-                <p className="text-footer">Desenvolvido por &lt;Amorim /&gt;</p>
+                <FiLoader color="#E02041" size={18} />
+                <p className="text-footer">Desenvolvido por
+                    <a href="https://portfolio-amorim.herokuapp.com/" target="_blank">
+                        &lt;Amorim /&gt;
+                    </a>
+                </p>
             </div>
         </div>
     )
